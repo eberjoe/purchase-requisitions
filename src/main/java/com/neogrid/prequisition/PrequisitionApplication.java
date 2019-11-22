632b9521ceb503e99e829c0e991b445fe8066c1c
Eber Martins Rodrigues <eber.rodrigues@neogrid.com>
2019-11-22 18:10:08 -0300

    Add Spring Init

---
+++ src/main/java/com/neogrid/prequisition/PrequisitionApplication.java
@@ -0,0 +1,13 @@
+package com.neogrid.prequisition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrequisitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrequisitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neogrid/prequisition/PrequisitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neogrid.prequisition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrequisitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
